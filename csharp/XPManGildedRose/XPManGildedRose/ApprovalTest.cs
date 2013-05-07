968e3a7b2d61900a36db5186e45c39fbff1d0dba
danielksmith <emaildanielksmith@gmail.com>
2013-05-07 09:57:03 +0100

    Commit C# Visual Studio 2010 project

---
+++ csharp/XPManGildedRose/XPManGildedRose/ApprovalTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using GildedRose;
+using NUnit.Framework;
+
+
+namespace GildedRoseTests
+{
+	[TestFixture]
+	public class ApprovalTest
+	{
+		[Test]
+		public void ThirtyDays()
+		{
+            FileStream fs = new FileStream(@"C:\Temp\XPManGildedRose\Output.txt", FileMode.Create);
+            TextWriter tmp = Console.Out;
+
+            StreamWriter sw = new StreamWriter(fs);
+            Console.SetOut(sw);
+
+			Program.Main(new string[] { });
+
+            sw.Close();
+		}
+	}
+	
+}
