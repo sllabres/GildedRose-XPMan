ec38200aac6f0ad826e44a4926805334f0a2e2ce
unknown <llabrs@GBBL1ITLP0143.drl.local>
2013-05-09 19:55:57 +0100

    add legendary item check

--- csharp/XPManGildedRose/XPManGildedRose/GildedRose.cs
+++ csharp/XPManGildedRose/XPManGildedRose/GildedRose.cs
@@ -14,44 +14,39 @@ namespace XPManGildedRose
 		{
 			for (var i = 0; i < Items.Count; i++)
 			{
-				if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-				{
-					if (Items[i].Quality > 0)
-					{
-						if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-						{
-							Items[i].Quality = Items[i].Quality - 1;
-						}
-					}
-				}
-				else
-				{
-					if (Items[i].Quality < 50)
-					{
-						Items[i].Quality = Items[i].Quality + 1;
-						
-						if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-						{
-							if (Items[i].SellIn < 11)
-							{
-								if (Items[i].Quality < 50)
-								{
-									Items[i].Quality = Items[i].Quality + 1;
-								}
-							}
-							
-							if (Items[i].SellIn < 6)
-							{
-								if (Items[i].Quality < 50)
-								{
-									Items[i].Quality = Items[i].Quality + 1;
-								}
-							}
-						}
-					}
-				}
-				
-				if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+			    if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" &&
+			        Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
+			    {
+			        Items[i].Quality = Items[i].Quality - 1;
+			    }
+			    else
+			    {
+			        if (Items[i].Quality < 50)
+			        {
+			            Items[i].Quality = Items[i].Quality + 1;
+
+			            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+			            {
+			                if (Items[i].SellIn < 11)
+			                {
+			                    if (Items[i].Quality < 50)
+			                    {
+			                        Items[i].Quality = Items[i].Quality + 1;
+			                    }
+			                }
+
+			                if (Items[i].SellIn < 6)
+			                {
+			                    if (Items[i].Quality < 50)
+			                    {
+			                        Items[i].Quality = Items[i].Quality + 1;
+			                    }
+			                }
+			            }
+			        }
+			    }
+
+			    if (IsLegendaryItem(Items[i].Name))
 				{
 					Items[i].SellIn = Items[i].SellIn - 1;
 				}
@@ -85,7 +80,11 @@ namespace XPManGildedRose
 				}
 			}
 		}
-		
+
+	    private bool IsLegendaryItem(string name)
+	    {
+	        return name != "Sulfuras, Hand of Ragnaros";
+	    }
 	}
 	
 	public class Item
