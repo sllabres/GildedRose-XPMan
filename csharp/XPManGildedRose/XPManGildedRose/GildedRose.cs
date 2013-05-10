d6e79b035fc8e71cbeb755a699d339e74d6ffa6f
unknown <llabrs@GBBL1ITLP0143.drl.local>
2013-05-10 09:07:32 +0100

    slight refactor

--- csharp/XPManGildedRose/XPManGildedRose/GildedRose.cs
+++ csharp/XPManGildedRose/XPManGildedRose/GildedRose.cs
@@ -14,61 +14,46 @@ namespace XPManGildedRose
 		{
 			for (var i = 0; i < Items.Count; i++)
 			{
-			    if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" &&
-			        Items[i].Quality > 0 && Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (IsLegendaryItem(Items[i].Name) == false && Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[i].Quality > 0)
 			    {
 			        Items[i].Quality = Items[i].Quality - 1;
 			    }
 			    else
 			    {
-			        if (Items[i].Quality < 50)
+                    if (Items[i].Quality < 50 && IsLegendaryItem(Items[i].Name) == false)
 			        {
 			            Items[i].Quality = Items[i].Quality + 1;
 
 			            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
 			            {
-			                if (Items[i].SellIn < 11)
+			                if (Items[i].SellIn < 11 && Items[i].Quality < 50)
 			                {
-			                    if (Items[i].Quality < 50)
-			                    {
-			                        Items[i].Quality = Items[i].Quality + 1;
-			                    }
+			                    Items[i].Quality = Items[i].Quality + 1;
 			                }
 
-			                if (Items[i].SellIn < 6)
+			                if (Items[i].SellIn < 6 && Items[i].Quality < 50)
 			                {
-			                    if (Items[i].Quality < 50)
-			                    {
-			                        Items[i].Quality = Items[i].Quality + 1;
-			                    }
+			                    Items[i].Quality = Items[i].Quality + 1;
 			                }
 			            }
 			        }
 			    }
 
-			    if (IsLegendaryItem(Items[i].Name))
-				{
-					Items[i].SellIn = Items[i].SellIn - 1;
-				}
-				
-				if (Items[i].SellIn < 0)
+                if (IsLegendaryItem(Items[i].Name) == false)
+				    Items[i].SellIn = Items[i].SellIn - 1;
+
+                if (Items[i].SellIn < 0 && IsLegendaryItem(Items[i].Name) == false)
 				{
 					if (Items[i].Name != "Aged Brie")
 					{
-						if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-						{
-							if (Items[i].Quality > 0)
-							{
-								if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-								{
-									Items[i].Quality = Items[i].Quality - 1;
-								}
-							}
-						}
-						else
-						{
-							Items[i].Quality = Items[i].Quality - Items[i].Quality;
-						}
+					    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && Items[i].Quality > 0)
+					    {
+					        Items[i].Quality = Items[i].Quality - 1;
+                        }
+					    else
+					    {
+					        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+					    }
 					}
 					else
 					{
@@ -83,7 +68,7 @@ namespace XPManGildedRose
 
 	    private bool IsLegendaryItem(string name)
 	    {
-	        return name != "Sulfuras, Hand of Ragnaros";
+	        return name == "Sulfuras, Hand of Ragnaros";
 	    }
 	}
 	
